b98e398c319890c7c4ea71e7417c3fb86b575b3e
arroyostack <arroyostack@gmail.com>
2024-01-05 14:19:55 +0100

    Feat: Esbuild-wasm service start implementation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import { useState } from "react";
+import * as esbuild from 'esbuild-wasm';
+import { useEffect, useRef, useState } from "react";
 
 
 function App() {
+  const ref = useRef<any>();
   const [input, setInput] = useState( '' );
   const [code, setCode] = useState( '' );
 
+  // With 'service.transform' we will transpile user input code and show the result
+  const startService = async () => {
+    ref.current = await esbuild.startService( {
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    } );
+  };
+
+  useEffect( () => {
+    startService();
+  }, [] );
+
   const handleClick = () => {
-    console.log( input );
+    if ( !ref.current ) return;
+    ref.current.transform( input, {
+      loader: 'jsx',
+      target: 'es2015'
+    } );
   };
 
   return (
@@ -20,6 +38,7 @@ function App() {
           onClick={ handleClick }
         >Submit</button>
       </div>
+
       <pre>{ code }</pre>
     </div>
   );
