d5242a9a0780426894cc3a8067303a01326038ff
arroyostack <arroyostack@gmail.com>
2024-01-09 12:18:38 +0100

    Feat: 'onResolve' function refactor and optimization.

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -11,26 +11,26 @@ export const unpkgPathPlugin = ( inputCode: string ) => {
   return {
     name: 'unpkg-path-plugin',
     setup( build: esbuild.PluginBuild ) {
+      // & Handles root entry file 'index.js'
+      build.onResolve( { filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      } );
+      // & Handles relative path in a module.
+      build.onResolve( { filter: /^\.+\// }, ( args: any ) => {
+        return {
+          namespace: 'a',
+          path: new URL(
+            args.path,
+            'https://unpkg.com' + args.resolveDir + '/'
+          ).href
+        };
+      } );
+      // & Handles main file of a module.
       build.onResolve( { filter: /.*/ }, async ( args: any ) => {
-        // console.log( 'onResolve', args );
-        if ( args.path === 'index.js' ) {
-          return { path: args.path, namespace: 'a' };
-        }
-        // Nested paths.
-        if ( args.path.includes( './' ) || args.path.includes( '../' ) ) {
-          return {
-            namespace: 'a',
-            path: new URL(
-              args.path,
-              'https://unpkg.com' + args.resolveDir + '/'
-            ).href
-          };
-        }
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
         };
-
       } );
 
       build.onLoad( { filter: /.*/ }, async ( args: any ) => {
